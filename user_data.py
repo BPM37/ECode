24e1bbc242a469ee661c76b1bbf8b10666eca888
BPM37 <142173862+BPM37@users.noreply.github.com>
2024-11-12 14:18:06 +0100

    Created user_data

---
+++ user_data.py
@@ -0,0 +1,94 @@
+nk = {'asc':{'deg':12,'sign':'AR','min':41,'syb':"A"},
+      'su': {'deg': 6, 'sign': 'TA', 'min': 1, 'syb': 'su'},
+      'mo': {'deg': 17, 'sign': 'AQ', 'min': 55, 'syb': 'mo'},
+       'me': {'deg': 8, 'sign': 'AR', 'min': 59, 'syb': 'me'},
+      've': {'deg': 23, 'sign': 'AR', 'min': 2, 'syb': 've'},
+       'ma': {'deg': 22, 'sign': 'PI', 'min': 23, 'syb': 'ma'},
+      'ju': {'deg': 4, 'sign': 'VI', 'min': 40, 'syb': 'ju'},
+       'sa': {'deg': 17, 'sign': 'AQ', 'min': 38, 'syb': 'sa'},
+      'ra': {'deg': 18, 'sign': 'CP', 'min': 0, 'syb': 'ra'},
+       'ke': {'deg': 18, 'sign': 'CN', 'min': 57, 'syb': 'ke'}}
+
+fe = {'asc':{'deg':16,'sign':'SG','min':15,'syb':"A"},
+         'su': {'deg': 0, 'sign': 'AR', 'min': 19, 'syb': 'su'},
+        'mo': {'deg': 27, 'sign': 'PI', 'min': 50, 'syb': 'mo'},
+       'me': {'deg': 17, 'sign': 'AR', 'min': 43, 'syb': 'me'},
+        've': {'deg': 21, 'sign': 'AR', 'min': 13, 'syb': 've'},
+       'ma': {'deg': 4, 'sign': 'TA', 'min': 12, 'syb': 'ma'},
+        'ju': {'deg': 8, 'sign': 'AQ', 'min': 59, 'syb': 'ju'},
+       'sa': {'deg': 27, 'sign': 'SC', 'min': 58, 'syb': 'sa'},
+        'ra': {'deg': 17, 'sign': 'SG', 'min': 59, 'syb': 'ra'},
+       'ke': {'deg': 17, 'sign': 'GE', 'min': 33, 'syb': 'ke'}}
+
+fe_w = {'asc':{'deg':17,'sign':'AR','min':8,'syb':"A"},
+        'su': {'deg': 26, 'sign': 'AR', 'min': 17, 'syb': 'su'},
+        'mo': {'deg': 1, 'sign': 'AR', 'min': 52, 'syb': 'mo'},
+        'me': {'deg': 14, 'sign': 'TA', 'min': 9, 'syb': 'me'},
+        've': {'deg': 11, 'sign': 'GE', 'min': 11, 'syb': 've'},
+        'ma': {'deg': 17, 'sign': 'AR', 'min': 17, 'syb': 'ma'},
+        'ju': {'deg': 17, 'sign': 'CP', 'min': 7, 'syb': 'ju'},
+        'sa': {'deg': 1, 'sign': 'AR', 'min': 1, 'syb': 'sa'},
+        'ra': {'deg': 16, 'sign': 'LI', 'min': 49, 'syb': 'ra'},
+        'ke': {'deg': 16, 'sign': 'AR', 'min': 49, 'syb': 'ke'}}
+
+confi = {'asc':{'deg':0,'sign':'AR','min':51,'syb':"A"},
+        'sun': {'deg': 5, 'sign': 'SG', 'min': 49, 'syb': 'sun'},
+        'mon': {'deg': 26, 'sign': 'AQ', 'min': 37, 'syb': 'mon'},
+        'mec': {'deg': 8, 'sign': 'SG', 'min': 48, 'syb': 'mec'},
+        'ven': {'deg': 1, 'sign': 'CP', 'min': 13, 'syb': 'ven'},
+        'mas': {'deg': 28, 'sign': 'SG', 'min': 27, 'syb': 'mas'},
+        'jup': {'deg': 21, 'sign': 'SG', 'min': 53, 'syb': 'jup'},
+        'sat': {'deg': 18, 'sign': 'PI', 'min': 2, 'syb': 'sat'},
+        'rah': {'deg': 25, 'sign': 'LI', 'min': 45, 'syb': 'rah'},
+        'ket': {'deg': 25, 'sign': 'AR', 'min': 45, 'syb': 'ket'}}
+
+pri = { 'asc':{'deg':16,'sign':'SG','min':15,'syb':"A"},
+        'su':{'deg':3,'sign':'PI','min':3,'syb':"su"},
+        'mo':{'deg':11,'sign':'AQ','min':3,'syb':"mo"},
+        #'mon':{'deg':14,'sign':'AR','min':18,'syb':"mon"},
+        'me':{'deg':6,'sign':'AQ','min':40,'syb':"me"},
+        've':{'deg':25,'sign':'CP','min':41,'syb':"ve"},
+        'ma':{'deg':19,'sign':'LI','min':10,'syb':"ma"},
+        'ju':{'deg':10,'sign':'SC','min':19,'syb':"ju"},
+        'sa':{'deg':21,'sign':'LI','min':50,'syb':"sa"},
+        'ra':{'deg':21,'sign':'CN','min':56,'syb':"ra"},
+        'ke':{'deg':21,'sign':'CP','min':56,'syb':"ke"},
+        #'p_fort':{'deg':8,'sign':'CP','min':15,'syb':"p_fort"},
+        }
+
+bet = {'asc': {'deg': 28, 'sign': 'TA', 'min': 25, 'syb': 'A'},
+       'su': {'deg': 13, 'sign': 'AQ', 'min': 6, 'syb': 'su'},
+       'mo': {'deg': 23, 'sign': 'AQ', 'min': 24, 'syb': 'mo'},
+       'me': {'deg': 24, 'sign': 'CP', 'min': 41, 'syb': 'me'},
+       've': {'deg': 11, 'sign': 'CP', 'min': 20, 'syb': 've'},
+       'ma': {'deg': 6, 'sign': 'CP', 'min': 10, 'syb': 'ma'},
+       'ju': {'deg': 7, 'sign': 'PI', 'min': 27, 'syb': 'ju'},
+       'sa': {'deg': 15, 'sign': 'AQ', 'min': 36, 'syb': 'sa'},
+       'ra': {'deg': 27, 'sign': 'TA', 'min': 50, 'syb': 'ra'},
+       'ke': {'deg': 27, 'sign': 'SC', 'min': 50, 'syb': 'ke'}}
+
+nanndy = {'asc': {'deg': 8, 'sign': 'AR', 'min': 2, 'syb': 'A'},
+          'su': {'deg': 7, 'sign': 'CN', 'min': 2, 'syb': 'su'},
+          'mo': {'deg': 8, 'sign': 'SC', 'min': 8, 'syb': 'mo'},
+          'me': {'deg': 4, 'sign': 'CN', 'min': 19, 'syb': 'me'},
+          've': {'deg': 19, 'sign': 'LE', 'min': 28, 'syb': 've'},
+          'ma': {'deg': 23, 'sign': 'LE', 'min': 4, 'syb': 'ma'},
+          'ju': {'deg': 8, 'sign': 'TA', 'min': 56, 'syb': 'ju'},
+          'sa': {'deg': 7, 'sign': 'PI', 'min': 6, 'syb': 'sa'},
+          'ra': {'deg': 0, 'sign': 'TA', 'min': 43, 'syb': 'ra'},
+          'ke': {'deg': 0, 'sign': 'SC', 'min': 43, 'syb': 'ke'}}
+
+jef = {'asc': {'deg': 21, 'sign': 'VI', 'min': 33, 'syb': 'A'},
+       'su': {'deg': 16, 'sign': 'LI', 'min': 35, 'syb': 'su'},
+       'mo': {'deg': 17, 'sign': 'SC', 'min': 1, 'syb': 'mo'},
+       'me': {'deg': 11, 'sign': 'LI', 'min': 21, 'syb': 'me'},
+       've': {'deg': 13, 'sign': 'SC', 'min': 10, 'syb': 've'},
+       'ma': {'deg': 17, 'sign': 'SC', 'min': 5, 'syb': 'ma'},
+       'ju': {'deg': 26, 'sign': 'PI', 'min': 1, 'syb': 'ju'},
+       'sa': {'deg': 8, 'sign': 'LI', 'min': 51, 'syb': 'sa'},
+       'ra': {'deg': 6, 'sign': 'CP', 'min': 42, 'syb': 'ra'},
+       'ke': {'deg': 6, 'sign': 'CN', 'min': 42, 'syb': 'ke'}}
+
+transit = {
+        'sat':{'deg':19, 'sign':'PI', 'min':29, 'syb':'tsat'} #to the materal world measurement
+}
