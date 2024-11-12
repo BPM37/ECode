2f3057e994a7e7bef3e852b035925f856828ea63
BPM37 <142173862+BPM37@users.noreply.github.com>
2024-11-12 13:53:56 +0100

    Created planet relationships

---
+++ R.py
@@ -0,0 +1,10 @@
+su = {'mo':'F','ju':'F','ma':'F','me':'E','ve':'N','sa':'N'}
+mo = {'me':'F','su':'F','ma':'N','ju':'N','sa':'N','ve':'N'}
+ma = {'su':'F','mo':'F','ju':'F','me':'E','ve':'N','sa':'N'}
+me = {'su':'F','ve':'F','mo':'E','ma':'N','ju':'N','sa':'N'}
+ju = {'mo':'F','ma':'F','su':'F','me':'E','ve':'E','sa':'N'}
+ve = {'me':'F','sa':'F','mo':'E','su':'E','ma':'N','ju':'N'}
+sa= {'me':'F','ve':'F','su':'E','mo':'E','ma':'E','ju':'N'}
+
+ra = {'ve':'F','sa':'E','me':'F','su':'E','mo':'N','ma':'N','ke':'E'}
+ke = {'su':'N','ma':'N','me':'E','sa':'E','ve':'N','mo':'N','ra':'E'}
